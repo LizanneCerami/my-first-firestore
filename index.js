e4b93c7d3a6f8a342092c4336ef111d7b0db9fc1
Lizanne Cerami <lizannecerami@gmail.com>
2023-03-29 15:49:33 -0400

    my first firestone

--- index.js
+++ index.js
@@ -14,16 +14,49 @@ initializeApp({
 const db = getFirestore();
 
 // Add a product (new document) to our products collection
-const candy = {
-  name: "Skittles",
-  unitPrice: 3.99,
-  size: "16 oz",
-  color: "green",
-  inventory: 144,
-  productNumber: 7,
+const candy2 = {
+  name: "Twix",
+  unitPrice:2.99,
+  size: "12 oz",
+  color: "gold",
+  inventory: 288,
+  productNumber: 2,
 }
 
-db.collection('products').add(candy)
-  .then(doc => console.log("added doc:" + doc.id))
-  .catch(err => console.log(err))
-  
+// How to add a document to Firestore:
+
+// db.collection('products').add(candy2) 
+// // Returns a promise, not a document.  A promise is a special kind of object in javaScript that returns "in progress" (by default), "resolved" (ran properly), or "rejected" (error occurred).
+// // While we are waiting for the promise...
+//   .then((doc) => {
+//     console.log("added doc:" + doc.id)
+//      // "resolves" = .then, I can be sure inside .then() that the first process was completed successfully
+//     })
+//     .catch(err => console.log(err)) // "rejected" = .catch
+
+// db.collection('products').doc('Rpl3M7wbAbGIG1b06eDI').delete() to delete a document, not used often.
+
+// How to update a document in Firestore: 
+db.collection('products').doc('Rpl3M7wbAbGIG1b06eDI').update({
+  inventory: 555,
+  customerFavorite: true
+})
+
+//How to read a document from Firestore:
+
+db.collection('products').doc('Rpl3M7wbAbGIG1b06eDI').get()
+.then(doc => {
+  console.log(doc.data());
+
+})
+.catch(err => console.log(err)) // .catch(console.log) does the same thing
+
+
+// How to get a whole collection:
+
+db.collection('products').get()
+  .then(collection => {
+    const productList = collection.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+    console.table(productList);
+  })
+  .catch(console.log);
